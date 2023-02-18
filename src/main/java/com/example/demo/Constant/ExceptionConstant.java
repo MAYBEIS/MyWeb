9534f6278621eac5862893df129a9b636584bbaa
Administrator <1913093102@qq.com>
2023-02-18 15:31:21 +0800

    注释测试

--- src/main/java/com/example/demo/Constant/ExceptionConstant.java
+++ src/main/java/com/example/demo/Constant/ExceptionConstant.java
@@ -1,7 +1,7 @@
 package com.example.demo.Constant;
 
 public class ExceptionConstant {
-
+    // 注释测试
     public static final String USER_LOGIN_FAIL = "用户名或者密码错误，登录失败";
     public static final String USER_NOT_EXIST = "用户不存在";
     public static final String ERROR_MSG = "全局异常";
